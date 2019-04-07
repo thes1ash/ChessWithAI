378d516401ec01f7d35e8dc27f1d9c65c910ec3d
Adam Opalecký <mail.opalecky@gmail.com>
2019-04-08 01:11:05 +0200

    Ver 2.0 - counting positions + added king and superClass

--- chessGame.js
+++ chessGame.js
@@ -1,36 +1,81 @@
+function translateVerticalPos(num) {
+	var letter = "Z";
+	switch (num) {
+		case 0:
+			letter = "A";
+			break;
+		case 1:
+			letter = "B";
+			break;
+		case 2:
+			letter = "C";
+			break;
+		case 3:
+			letter = "D";
+			break;
+		case 4:
+			letter = "E";
+			break;
+		case 5:
+			letter = "F";
+			break;
+		case 6:
+			letter = "G";
+			break;
+		case 7:
+			letter = "H";
+			break;
+		default:
+			letter = "Z";
+			break;
+	}
+	return letter;
+}
+
 $("#board").find("tr").each(function() {
 	for (i = 0; i < 8; i++) {
-		switch (i) {
-			case 0:
-				var letter = "A";
-				break;
-			case 1:
-				var letter = "B";
-				break;
-			case 2:
-				var letter = "C";
-				break;
-			case 3:
-				var letter = "D";
-				break;
-			case 4:
-				var letter = "E";
-				break;
-			case 5:
-				var letter = "F";
-				break;
-			case 6:
-				var letter = "G";
-				break;
-			case 7:
-				var letter = "H";
-				break;
-			default:
-				var letter = "Z";
-				break;
-
-		}
-		$(this).append('<td id=' + letter + $(this).attr("id") + '"></td>');
-
+		$(this).append(
+			'<td id=' +
+			translateVerticalPos(i) +
+			$(this).attr("id") +
+			'"></td>'
+		);
 	}
 })
+
+function countMovements(origX, origY, vertical, horizontal, diagonal, knight) {
+	if (knight == 1) {
+		newPositions[8] = {};
+	} else {
+		newPositions[(vertical) + (horizontal) + (diagonal)] = {};
+	}
+	if (vertical > 0) {
+		for (i = 0; i < 2 * vertical; i++) {
+			while (i < vertical) {
+				newPosition[].push(stranslateVerticalPos(origY - i) + origX);
+			}
+			newPosition[].push(translateVerticalPos(origY + (i - vertical)) + origX);
+		}
+	}
+}
+
+class Pieces {
+	constructor(color, positionX, positionY) {
+		this.positionY = positionY - 1;
+		this.positionX = positionX - 1;
+	}
+	taken(boolean) {
+		if (boolean == 0) {
+			let isTaken = false;
+		} else {
+			let isTaken = true;
+		}
+	}
+
+}
+class King extends Pieces {
+	constructor(color, positionX, positionY) {
+		let isTaken = false;
+
+	}
+}
